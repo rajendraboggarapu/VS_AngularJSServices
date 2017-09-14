216dd8e4d19b33dc148e5e5ce607e4a45f914c13
J  V G Naga Rajendra Boggarapu <30888973+rajendraboggarapu@users.noreply.github.com>
2017-09-14 19:40:23 +0530

    intial Commit

---
+++ AngularTestService/Authontication.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using System.ServiceModel.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AngularTestService
+{
+    public class Authontication
+    {
+        [WebInvoke(Method = "GET",
+                    ResponseFormat = WebMessageFormat.Json,
+                    UriTemplate = "data/{id}")]
+        public DataSet Authonticate(string uname, string upwd)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                
+                string strcon = ConfigurationManager.ConnectionStrings["ConnectionLocaldb"].ToString();
+                SqlConnection conn = new SqlConnection(strcon);
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "usp_getUserdetails";
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add("@username", SqlDbType.VarChar).Value = uname;
+                cmd.Parameters.Add("@password", SqlDbType.VarChar).Value = upwd;
+               
+                cmd.Connection = conn;
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+                da.Fill(ds);
+                conn.Close();
+
+
+               
+            }
+            catch(Exception ex)
+            {
+                
+            }
+
+            return ds;
+        }
+
+
+        public class Userdetails
+        {
+            public string uname { get; set; }
+            public string upwd { get; set; }
+        }
+
+    }
+}
